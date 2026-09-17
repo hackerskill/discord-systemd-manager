5d0b0b7717e9ce1c782c0c8a0b809797343a1078
hackerskills <67186714+hackerskill@users.noreply.github.com>
2026-09-17 12:20:49 +0530

    implemented status command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from unittest import result
 from dotenv import load_dotenv
 import discord
 from discord import app_commands
@@ -44,25 +45,35 @@ async def services(interaction: discord.Interaction):
     await interaction.followup.send("\n".join(services_list))
 
 @tree.command(name="restart", description="Restart a service on the server")
-@app_commands.choices(services=services_choices)
+@app_commands.choices(service=services_choices)
 async def restart(interaction: discord.Interaction, service: app_commands.Choice[str]):
     await interaction.response.send_message(f"Restarting {service.value}...")
     subprocess.run(["systemctl", "restart", service.value])
     await interaction.followup.send("Service restarted successfully.")
 
 @tree.command(name="start", description="Start any service running on the server")
-@app_commands.choices(services=services_choices)
-async def start(interaction: discord.Interaction, services: app_commands.Choice[str]):
-    await interaction.response.send_message(f"Starting {services.value}...")
-    subprocess.run(["systemctl", "start", services.value])
+@app_commands.choices(service=services_choices)
+async def start(interaction: discord.Interaction, service: app_commands.Choice[str]):
+    await interaction.response.send_message(f"Starting {service.value}...")
+    subprocess.run(["systemctl", "start", service.value])
     await interaction.followup.send("Service started successfully.")
 
 @tree.command(name="stop", description="Stop a service running on the server")
-@app_commands.choices(services=services_choices)
-async def stop(interaction: discord.Interaction, services: app_commands.Choice[str]):
-    await interaction.response.send_message(f"Stopping {services.value}...")
-    subprocess.run(["systemctl", "stop", services.value])
+@app_commands.choices(service=services_choices)
+async def stop(interaction: discord.Interaction, service: app_commands.Choice[str]):
+    await interaction.response.send_message(f"Stopping {service.value}...")
+    subprocess.run(["systemctl", "stop", service.value])
     await interaction.followup.send("Service stopped successfully.")
+
+@tree.command(name="status", description="See status of any service running on the server")
+@app_commands.choices(service=services_choices)
+async def status(interaction: discord.Interaction, service: app_commands.Choice[str]):
+    await interaction.response.send_message(f"Checking status of {service.value}...")
+    result = subprocess.run(["systemctl", "status", service.value], capture_output=True, text=True)
+    output = result.stdout.strip()
+    if result.stderr.strip():
+        output += f"\n{result.stderr.strip()}"
+    await interaction.followup.send(f"```text\n{output}\n```")
 
 @tree.command(name="about", description="Get information about this bot")
 async def about(interaction: discord.Interaction):
